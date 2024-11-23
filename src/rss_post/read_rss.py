4b1d1baa733d1fca862dd1cc58ff1a5f7ae8c99f
Chapin Bryce <mail@chapinb.com>
2024-11-23 12:41:30 -0500

    R - Extract method for reading the full feed

--- src/rss_post/read_rss.py
+++ src/rss_post/read_rss.py
@@ -1,6 +1,13 @@
 import feedparser
 
+from feedparser import FeedParserDict
+
+
+def read_rss(url) -> FeedParserDict:
+    feed = feedparser.parse(url)
+    return feed
+
 
 def read_rss_items(url) -> list[dict]:
-    feed = feedparser.parse(url)
+    feed = read_rss(url)
     return feed.entries
