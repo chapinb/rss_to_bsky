12085df5c5934bf92b12f5ea20e6518ff389178f
Chapin Bryce <mail@chapinb.com>
2024-11-23 15:35:11 -0500

    F!! Update feeds for Norwalk

--- src/rss_post/norwalk_feeds.py
+++ src/rss_post/norwalk_feeds.py
@@ -4,12 +4,28 @@ from rss_post.post import Post
 from rss_post.read_rss import read_rss_items
 
 
-def generate_calendar_events(feed_url: str) -> list[client_utils.TextBuilder]:
+def generate_calendar_events(
+    feed_name: str, feed_url: str
+) -> list[client_utils.TextBuilder]:
     items = read_rss_items(feed_url)
     return [
         Post()
-        .from_feed("Norwalk CT City Calendar")
+        .from_feed(feed_name)
         .with_title(item.title)
+        .with_description(item.description)
+        .with_link("Read more", item.link)
+        .build()
+        for item in items
+    ]
+
+
+def generate_without_title(
+    feed_name: str, feed_url: str
+) -> list[client_utils.TextBuilder]:
+    items = read_rss_items(feed_url)
+    return [
+        Post()
+        .from_feed(feed_name)
         .with_description(item.description)
         .with_link("Read more", item.link)
         .build()
@@ -19,5 +35,13 @@ def generate_calendar_events(feed_url: str) -> list[client_utils.TextBuilder]:
 
 def get_committee_events() -> list[client_utils.TextBuilder]:
     return generate_calendar_events(
-        "https://www.norwalkct.gov/RSSFeed.aspx?ModID=58&CID=Calendar-of-Agency-Board-Commission-Comm-47"
+        "City of Norwalk CT Calendar",
+        "https://www.norwalkct.gov/RSSFeed.aspx?ModID=58&CID=Calendar-of-Agency-Board-Commission-Comm-47",
+    )
+
+
+def get_news_flashes() -> list[client_utils.TextBuilder]:
+    return generate_without_title(
+        "City of Norwalk CT News",
+        "https://www.norwalkct.gov/RSSFeed.aspx?ModID=1&CID=All-newsflash.xml",
     )
